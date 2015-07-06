a66bf0ede59656efa9ae26abd208ac219e6f74ce
agz1990 <522360568@qq.com>
2015-07-07 00:37:25 +0800

    1.修复文件后缀缺失的问题

--- tutorial/tutorial/spiders/dmoz_spider.py
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -20,7 +20,7 @@ class DomzSpider(scrapy.Spider):
     ]
     
     def parse(self, response):
-        filename = response.url.split("/")[-2] + 'html'
+        filename = response.url.split("/")[-2] + '.html'
         with open(filename, 'wb') as f:
             f.write(response.body)
             
