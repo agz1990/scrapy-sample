5ac3bc604f434b9f0264f1043add0d77d405300d
agz1990 <522360568@qq.com>
2015-07-07 00:35:39 +0800

    爬虫测试通过 注意在windows环境下需要安装  win32api 第三方库： http://sourceforge.net/projects/pywin32/?source=typ_redirect

---
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -0,0 +1,26 @@
+'''
+Created on 2015-07-07
+
+@author: jigc
+'''
+
+import scrapy
+
+
+class DomzSpider(scrapy.Spider):
+    '''
+    classdocs
+    '''
+
+    name = 'dmoz'
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
+    
+    def parse(self, response):
+        filename = response.url.split("/")[-2] + 'html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+            
